b3bc7d40545bfde8356efe3fea0307d10440ace3
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-06 23:22:22 +0530

    create two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import OverviewPage from "./pages/OverviewPage"
+import ProductsPage from "./pages/ProductsPage"
+
 function App() {
 
   return (
-    <div className='text-red-500 text-4xl bg-blue-500'>
-      Hello World !
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 opacity-80">
+          <div className="">
+
+          </div>
+        </div>
+      </div>
+
+
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </div>
   )
 
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OverviewPage = () => {
+  return (
+    <div>
+      Overview
+    </div>
+  )
+}
+
+export default OverviewPage
