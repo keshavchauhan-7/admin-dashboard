9bba91290f5780a8049e6a091ffc9e73fba40b8c
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-09-09 17:54:40 +0530

    overview page complete

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
 import SalesOverviewChart from "../components/overview/SalesOverviewChart";
-// import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
-// import SalesChannelChart from "../components/overview/SalesChannelChart";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../components/overview/SalesChannelChart";
 
 const OverviewPage = () => {
   return (
@@ -30,8 +30,8 @@ const OverviewPage = () => {
 
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
           <SalesOverviewChart />
-          {/* <CategoryDistributionChart />
-          <SalesChannelChart /> */}
+          <CategoryDistributionChart />
+          <SalesChannelChart />
         </div>
       </main>
     </div>
